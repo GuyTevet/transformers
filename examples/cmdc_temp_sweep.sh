4539eca9e9b96f520b6377c8b2f086da769480b5
GuyTevet <guy.tvt@gmail.com>
2020-02-14 14:42:15 +0200

    cmdc - randomize script

--- examples/cmdc_temp_sweep.sh
+++ examples/cmdc_temp_sweep.sh
@@ -8,10 +8,13 @@ if [ ! -e $DIR ]; then
     mkdir $DIR
 fi
 
+seed=0
+
 for t in "${TEMPS[@]}"; do
+        ((seed+=1))
         OUTPATH=./${DIR}/output.cmdc.uni.sample${SAMPLES}.temp${t}
 
         python run_generation_cmdc.py  --model_type gpt2 --model_name gpt2-large\
             --prompts_path cmdc_processed/test.enc.prompt.txt --output_path $OUTPATH\
-            --length 20 --p=1. --temperature $t --samples_per_prompt $SAMPLES --seed 777
+            --length 20 --p=1. --temperature $t --samples_per_prompt $SAMPLES --seed $seed
     done
