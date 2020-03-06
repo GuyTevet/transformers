921969a2ebd4f87b7259a629ddcc221dbd374635
GuyTevet <guy.tvt@gmail.com>
2020-03-06 19:16:59 +0200

    cmdc - p sweep script - fix

--- examples/cmdc_p_sweep.sh
+++ examples/cmdc_p_sweep.sh
@@ -12,7 +12,7 @@ seed=0
 
 for p in "${TOPP[@]}"; do
         ((seed+=1))
-        OUTPATH=./${DIR}/output.cmdc.uni.sample${SAMPLES}.temp${t}
+        OUTPATH=./${DIR}/output.cmdc.uni.sample${SAMPLES}.topp${p}
 
         python run_generation_cmdc.py  --model_type gpt2 --model_name gpt2-large\
             --prompts_path cmdc_processed/test.enc.prompt.txt --output_path $OUTPATH\
