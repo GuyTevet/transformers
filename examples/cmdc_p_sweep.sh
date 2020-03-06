edb0461703c1af4cd0ab93229c5eb3fe520edc36
GuyTevet <guy.tvt@gmail.com>
2020-03-06 18:17:28 +0200

    cmdc - p sweep script

--- examples/cmdc_p_sweep.sh
+++ examples/cmdc_p_sweep.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 SAMPLES=10
-TEMPS=($(seq 0.1 0.01 1.11))
-DIR=cmdc_temp_sweep
+TOPP=($(seq 0.1 0.009 1.0))
+DIR=cmdc_p_sweep
 
 if [ ! -e $DIR ]; then
     mkdir $DIR
@@ -10,11 +10,11 @@ fi
 
 seed=0
 
-for t in "${TEMPS[@]}"; do
+for p in "${TOPP[@]}"; do
         ((seed+=1))
         OUTPATH=./${DIR}/output.cmdc.uni.sample${SAMPLES}.temp${t}
 
         python run_generation_cmdc.py  --model_type gpt2 --model_name gpt2-large\
             --prompts_path cmdc_processed/test.enc.prompt.txt --output_path $OUTPATH\
-            --length 20 --p=1. --temperature $t --samples_per_prompt $SAMPLES --seed $seed
+            --length 20 --p $p --temperature 1. --samples_per_prompt $SAMPLES --seed $seed
     done
